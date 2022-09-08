bba97ec226bd344ee6e4517b3503a4b6c70edd2c
Amka <Aminhassen1470@gmail.com>
2022-09-08 01:55:52 -0400

    create todosData

---
+++ src/components/todos/todosData.js
@@ -0,0 +1,32 @@
+export default [
+	{
+		id: 1,
+		text: 'Item One. Lorem ipsum dolor, sit amet Repellendus, neque?',
+		isCompleted: false,
+		isEditing: false,
+	},
+	{
+		id: 2,
+		text: 'Item Two. Lorem ipsum dolor sit amet.',
+		isCompleted: false,
+		isEditing: false,
+	},
+	{
+		id: 3,
+		text: 'Item Three. Lorem ipsum dolor sit amet consectetur.',
+		isCompleted: true,
+		isEditing: false,
+	},
+	{
+		id: 4,
+		text: 'Item Four. Lorem ipsum dolor sit adipisicing.',
+		isCompleted: false,
+		isEditing: false,
+	},
+	{
+		id: 5,
+		text: 'Item Five. Lorem ipsum dolor sit amet.',
+		isCompleted: false,
+		isEditing: false,
+	},
+];
