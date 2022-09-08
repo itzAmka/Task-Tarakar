44e4c72d8a7fdd825bd92b2a7749d690de5a6494
Amka <Aminhassen1470@gmail.com>
2022-09-08 01:58:00 -0400

    create unique id and use it in todosData

--- src/components/todos/todosData.js
+++ src/components/todos/todosData.js
@@ -1,30 +1,32 @@
+import { uid } from '../../helpers/uid';
+
 export default [
 	{
-		id: 1,
+		id: uid(),
 		text: 'Item One. Lorem ipsum dolor, sit amet Repellendus, neque?',
 		isCompleted: false,
 		isEditing: false,
 	},
 	{
-		id: 2,
+		id: uid(),
 		text: 'Item Two. Lorem ipsum dolor sit amet.',
 		isCompleted: false,
 		isEditing: false,
 	},
 	{
-		id: 3,
+		id: uid(),
 		text: 'Item Three. Lorem ipsum dolor sit amet consectetur.',
 		isCompleted: true,
 		isEditing: false,
 	},
 	{
-		id: 4,
+		id: uid(),
 		text: 'Item Four. Lorem ipsum dolor sit adipisicing.',
 		isCompleted: false,
 		isEditing: false,
 	},
 	{
-		id: 5,
+		id: uid(),
 		text: 'Item Five. Lorem ipsum dolor sit amet.',
 		isCompleted: false,
 		isEditing: false,
