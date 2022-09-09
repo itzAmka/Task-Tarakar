2533b132624a79d7823628136b8f4038acdc8593
Amka <Aminhassen1470@gmail.com>
2022-09-09 19:20:48 -0400

    use add to function in AddTodoForm

--- src/components/todos/AddTodoForm.jsx
+++ src/components/todos/AddTodoForm.jsx
@@ -1,7 +1,28 @@
+import { useContext, useState } from 'react';
+import { TodosContext } from '../../context/TodosContext';
+
 const AddTodoForm = () => {
+	const { addTodo } = useContext(TodosContext);
+	const [text, setText] = useState('');
+
+	const handleChange = e => {
+		setText(e.target.value);
+	};
+
+	const handleSubmit = e => {
+		e.preventDefault();
+		addTodo(text);
+		setText('');
+	};
+
 	return (
-		<form className='w-full flex gap-3 mt-12'>
-			<input type='text' className='input input-primary grow' />
+		<form onSubmit={handleSubmit} className='w-full flex gap-3 mt-12'>
+			<input
+				type='text'
+				value={text}
+				onChange={handleChange}
+				className='input input-primary grow'
+			/>
 			<button className='btn btn-primary'>Add Todo</button>
 		</form>
 	);
