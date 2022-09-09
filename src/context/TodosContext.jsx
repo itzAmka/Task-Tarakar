dbdb452aa13108ffb328785a0962118a651421b3
Amka <Aminhassen1470@gmail.com>
2022-09-09 19:19:45 -0400

    create addTodo function

--- src/context/TodosContext.jsx
+++ src/context/TodosContext.jsx
@@ -1,13 +1,30 @@
 import { createContext } from 'react';
 import { useLocalStorage } from '../hooks/useLocalStorage';
 import todosData from '../data/todosData';
+import { uid } from '../helpers/uid';
 
 export const TodosContext = createContext();
 
 export const TodosProvider = ({ children }) => {
 	const [todos, setTodos] = useLocalStorage('todos', todosData);
 
+	const addTodo = text => {
+		setTodos(currState => {
+			return [
+				...currState,
+				{
+					id: uid(),
+					text,
+					isCompleted: false,
+					isEditing: false,
+				},
+			];
+		});
+	};
+
 	return (
-		<TodosContext.Provider value={{ todos }}>{children}</TodosContext.Provider>
+		<TodosContext.Provider value={{ todos, addTodo }}>
+			{children}
+		</TodosContext.Provider>
 	);
 };
