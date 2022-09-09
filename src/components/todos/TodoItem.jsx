67f5a51716d6341af96e8c29f3ebfc89361afc3c
Amka <Aminhassen1470@gmail.com>
2022-09-09 19:41:40 -0400

    create conrol button for the todoItem text

--- src/components/todos/TodoItem.jsx
+++ src/components/todos/TodoItem.jsx
@@ -1,3 +1,7 @@
+import { MdDelete } from 'react-icons/Md';
+import { FaEdit } from 'react-icons/fa';
+import { BsFillCheckCircleFill } from 'react-icons/bs';
+
 const TodoItem = ({ todoItem }) => {
 	const { id, text, isCompleted, isEditing } = todoItem;
 
@@ -13,8 +17,21 @@ const TodoItem = ({ todoItem }) => {
 					/>
 				</form>
 			) : (
-				<span>{text}</span>
+				<span className={`${isCompleted ? 'line-through' : ''}`}>{text}</span>
 			)}
+			<span className='flex items-center gap-2'>
+				<button
+					disabled={isCompleted}
+					className='disabled:text-gray-500 text-yellow-500'>
+					<FaEdit size={20} />
+				</button>
+				<button className='text-green-500 ml-1'>
+					<BsFillCheckCircleFill size={19} />
+				</button>
+				<button className='text-red-500'>
+					<MdDelete size={23} />
+				</button>
+			</span>
 		</li>
 	);
 };
