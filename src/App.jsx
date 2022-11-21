3da48a0523f6b193dc34e320c17282d0439b7cf6
AmkaE <94766167+AmkaE@users.noreply.github.com>
2022-11-21 03:01:00 +0000

    create home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,28 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Container from './components/Container';
 import Navbar from './components/Navbar';
-import AddTodoForm from './components/todos/AddTodoForm';
-import TodosList from './components/todos/TodosList';
 import { TodosProvider } from './context/TodosContext';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.min.css';
+import Home from './pages/home';
 
 const App = () => {
 	return (
-		<TodosProvider>
-			<Container>
-				<Navbar />
-				<AddTodoForm />
-				<TodosList />
-			</Container>
-			<ToastContainer
-				position='top-center'
-				autoClose={1000}
-				className='sm:mt-10 mt-5 px-5'
-			/>
-		</TodosProvider>
+		<Router>
+			<TodosProvider>
+				<Container>
+					<Navbar />
+					<Routes>
+						<Route path='/' element={<Home />} />
+					</Routes>
+				</Container>
+				<ToastContainer
+					position='top-center'
+					autoClose={1000}
+					className='sm:mt-10 mt-5 px-5'
+				/>
+			</TodosProvider>
+		</Router>
 	);
 };
 
---
+++ src/pages/home.jsx
@@ -0,0 +1,13 @@
+import AddTodoForm from '../components/todos/AddTodoForm';
+import TodosList from '../components/todos/TodosList';
+
+const Home = () => {
+	return (
+		<>
+			<AddTodoForm />
+			<TodosList />
+		</>
+	);
+};
+
+export default Home;
