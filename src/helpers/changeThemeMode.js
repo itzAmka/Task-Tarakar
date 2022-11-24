ca479c94564580cd332f787292f7b5cf6a98fbd3
Amka <Aminhassen1470@gmail.com>
2022-11-24 17:37:16 -0500

    add default theme mode option in the changeThemeMode helper function

--- src/helpers/changeThemeMode.js
+++ src/helpers/changeThemeMode.js
@@ -1,5 +1,5 @@
 export const changeThemeMode = themeMode => {
 	const dataTheme = document.querySelector('[data-theme]');
 	const currentTheme = dataTheme.attributes['data-theme'];
-	currentTheme.value = JSON.parse(localStorage.getItem(themeMode));
+	currentTheme.value = JSON.parse(localStorage.getItem(themeMode)) || 'night';
 };
