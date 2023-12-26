ae3ec1c4ab91aa363d76e853c5a664da0fea0762
Amka <Aminhassen1470@gmail.com>
2023-12-26 10:48:10 -0500

    update: add types to `SignIn` component page

--- src/pages/sign-in.tsx
+++ src/pages/sign-in.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { type ChangeEvent, type FormEvent, useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { BsEyeFill, BsEyeSlashFill } from 'react-icons/bs';
 import { MdEmail } from 'react-icons/md';
@@ -24,14 +24,14 @@ const SignIn = () => {
 		setShowPassword(prevState => !prevState);
 	};
 
-	const handleChange = e => {
+	const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
 		setFormData(prevState => ({
 			...prevState,
 			[e.target.id]: e.target.value,
 		}));
 	};
 
-	const handleSubmit = async e => {
+	const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
 		e.preventDefault();
 
 		if (name !== '' || email !== '' || password !== '') {
