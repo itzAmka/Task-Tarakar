f8a37f1670eae58d084fbd68f976debc408b79fa
Amka <Aminhassen1470@gmail.com>
2023-12-26 09:04:00 -0500

    add types to `Container` component

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,4 +1,8 @@
-const Container = ({ children }) => {
+import { ComponentProps } from "react";
+
+type ContainerProps = ComponentProps<"div">;
+
+const Container = ({ children }: ContainerProps) => {
 	return (
 		<div className='container mx-auto max-w-3xl mt-5 px-3'>{children}</div>
 	);
