9c8c699299fe3bc7c323999ce09f89fb3b46bbe6
Amka <Aminhassen1470@gmail.com>
2023-12-26 10:02:40 -0500

    update: `useLocalStorage` fix types issue

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -1,13 +1,13 @@
-import { useEffect, useState } from 'react';
+import  { type Dispatch, type SetStateAction, useEffect, useState } from 'react';
 
-export const useLocalStorage = <T>(key: string, initialValue: T | (() => T)) => {
-	const [value, setValue] = useState<T>(() => {
+export const useLocalStorage = <T>(key: string, initialValue: T | (() => T)): [T, Dispatch<SetStateAction<T>>] => {
+	const [storedValue, setStoredValue] = useState<T>(() => {
 		// find the value from the localStorage
 		const jsonValue = localStorage.getItem(key);
 
 		// if the value is found the parse and return it
 		if (jsonValue !== null) {
-			return JSON.parse(jsonValue);
+			return JSON.parse(jsonValue) as T;
 		}
 
 		// but if the initial value is a function, invoke it.
@@ -16,13 +16,13 @@ export const useLocalStorage = <T>(key: string, initialValue: T | (() => T)) => 
 		}
 
 		// else if the initial value is is not a function and not found from the localStorage return it
-		return initialValue;
+		return initialValue as T;
 	});
 
 	// update the localStorage every time the key and value changes
 	useEffect(() => {
-		localStorage.setItem(key, JSON.stringify(value));
-	}, [key, value]);
+		localStorage.setItem(key, JSON.stringify(storedValue))
+	}, [key, storedValue]);
 
-	return [value, setValue];
+	return [storedValue, setStoredValue];
 };
