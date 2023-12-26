c0192d6cfe0ae2b9cfaebc071bc2093993d8265e
Amka <Aminhassen1470@gmail.com>
2023-12-26 10:14:00 -0500

    update: remove console log

--- src/helpers/changeThemeMode.ts
+++ src/helpers/changeThemeMode.ts
@@ -3,10 +3,7 @@ import { getCurrentTheme } from "./getCurrentTheme";
 export const changeThemeMode = (themeMode: string) => {
 	let currentTheme = getCurrentTheme();
 
-
 	const localStorageTheme = localStorage.getItem(themeMode);
-
-	console.log('localStorageTheme', localStorageTheme)
 
 	if (!localStorageTheme) return;
 
