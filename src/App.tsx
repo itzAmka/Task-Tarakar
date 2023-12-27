6122fb86644ccd86aa58281f328ea2b9dcd471d5
Amka <Aminhassen1470@gmail.com>
2023-12-27 12:03:46 -0500

    set up routes with the new `react-router-dom` version

--- src/App.tsx
+++ src/App.tsx
@@ -1,61 +1,80 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { useEffect } from 'react'
+
+/* -------- react router dom -------- */
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
 /* --------- react toastify --------- */
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.min.css'
 
-/* ----------- components ----------- */
-import Container from '@components/shared/Container.tsx'
-import Navbar from '@components/Navbar.tsx'
+/* -------------- layouts ------------ */
+import RootLayout from '@layouts/RootLayout.tsx'
+import AuthLayout from '@layouts/AuthLayout.tsx'
 
 /* -------------- pages ------------- */
-import Home from '@pages/home.tsx'
+import Dashboard from '@pages/dashboard.tsx'
+import Tasks from '@pages/tasks.tsx'
 import Settings from '@pages/settings.tsx'
 import SignIn from '@pages/sign-in.tsx'
 import SignUp from '@pages/sign-up.tsx'
 import ForgotPassword from '@pages/forgot-password.tsx'
 
-/* --------- routes protection -------- */
-import AuthenticatedRoutes from '@components/AuthenticatedRoutes.tsx'
-import UnAuthenticatedRoutes from '@components/UnAuthenticatedRoutes.tsx'
-
 /* ------------- helpers ------------ */
 import { changeThemeMode } from '@helpers/changeThemeMode.ts'
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <RootLayout />,
+    children: [
+      {
+        path: '/',
+        element: <Dashboard />,
+      },
+      {
+        path: 'tasks',
+        element: <Tasks />,
+      },
+      {
+        path: 'settings',
+        element: <Settings />,
+      },
+    ],
+  },
+  {
+    path: '/',
+    element: <AuthLayout />,
+    children: [
+      {
+        path: 'sign-in',
+        element: <SignIn />,
+      },
+      {
+        path: 'sign-up',
+        element: <SignUp />,
+      },
+      {
+        path: 'forgot-password',
+        element: <ForgotPassword />,
+      },
+    ],
+  },
+])
+
 const App = () => {
-  changeThemeMode('selectedOptionTheme')
+  useEffect(() => {
+    changeThemeMode('selectedOptionTheme')
+  }, [])
 
   return (
-    <Router>
-      <Container>
-        <Navbar />
-        <Routes>
-          {/* home route: only for authenticated users */}
-          <Route path='/' element={<AuthenticatedRoutes />}>
-            <Route path='/' element={<Home />} />
-          </Route>
-          {/* settings route: only for authenticated users */}
-          <Route path='/settings' element={<AuthenticatedRoutes />}>
-            <Route path='/settings' element={<Settings />} />
-          </Route>
-          {/* sign-in route: only for unauthenticated users */}
-          <Route path='/sign-in' element={<UnAuthenticatedRoutes />}>
-            <Route path='/sign-in' element={<SignIn />} />
-          </Route>
-          {/* sign-up route: only for unauthenticated user */}
-          <Route path='/sign-up' element={<UnAuthenticatedRoutes />}>
-            <Route path='/sign-up' element={<SignUp />} />
-          </Route>
-          {/* forgot-password route: authenticated and unauthenticated users can access to reset their password */}
-          <Route path='/forgot-password' element={<ForgotPassword />} />
-        </Routes>
-      </Container>
+    <>
+      <RouterProvider router={router}></RouterProvider>
       <ToastContainer
         position='top-center'
         autoClose={1000}
         className='sm:mt-10 mt-5 px-5'
       />
-    </Router>
+    </>
   )
 }
 
