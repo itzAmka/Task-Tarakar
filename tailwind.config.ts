e077deb391d83e4b2f7238cdaeaf030b9c0d0155
Amka <Aminhassen1470@gmail.com>
2023-12-27 09:54:58 -0500

    fix: daisyui issue

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,27 +1,29 @@
+import daisyui from 'daisyui'
+
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [require('daisyui')],
-	daisyui: {
-		themes: [
-			'light',
-			'dark',
-			'synthwave',
-			'retro',
-			'cyberpunk',
-			'valentine',
-			'halloween',
-			'forest',
-			'aqua',
-			'wireframe',
-			'black',
-			'luxury',
-			'dracula',
-			'night',
-			'coffee',
-		],
-	},
-};
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [daisyui],
+  daisyui: {
+    themes: [
+      'light',
+      'dark',
+      'synthwave',
+      'retro',
+      'cyberpunk',
+      'valentine',
+      'halloween',
+      'forest',
+      'aqua',
+      'wireframe',
+      'black',
+      'luxury',
+      'dracula',
+      'night',
+      'coffee',
+    ],
+  },
+}
