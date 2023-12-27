d9c5c5bc3f01308c89797c429447bb30b03f9905
Amka <Aminhassen1470@gmail.com>
2023-12-27 17:25:24 -0500

    update categories schema

--- src/zod/categoriesSchema.ts
+++ src/zod/categoriesSchema.ts
@@ -1,11 +1,12 @@
-import { z } from 'zod';
+import { z } from 'zod'
 
 export const categorySchema = z.object({
   id: z.string(),
   title: z.string(),
-  userRef: z.string(),
+  color: z.string().regex(/^#[0-9a-f]{6}$/i, { message: 'Invalid color' }),
+  description: z.string(),
   updatedAt: z.string(),
   createdAt: z.string(),
-});
+})
 
-export type Category = z.infer<typeof categorySchema>;
+export type Category = z.infer<typeof categorySchema>
