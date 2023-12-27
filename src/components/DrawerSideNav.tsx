9c851cdbc0f7b5140afc94aa656ae1cabaff79e0
Amka <Aminhassen1470@gmail.com>
2023-12-27 12:32:03 -0500

    create `DrawerSideNav` component

---
+++ src/components/DrawerSideNav.tsx
@@ -0,0 +1,48 @@
+import { type ComponentProps } from 'react'
+import { NavLink } from 'react-router-dom'
+import { FaTasks } from 'react-icons/fa'
+import { FaGauge, FaGear } from 'react-icons/fa6'
+import Logo from '@components/shared/Logo'
+
+type DrawerSideNavProps = ComponentProps<'div'> & {
+  closeDrawer?: () => void
+}
+
+const DrawerSideNav = ({ closeDrawer }: DrawerSideNavProps) => {
+  return (
+    <>
+      <ul className='menu gap-2 p-4 w-fit min-h-full bg-base-300 text-base-content'>
+        {/* Sidebar content here */}
+        <li className='mb-10'>
+          <Logo closeDrawer={closeDrawer} />
+        </li>
+        <li className=''>
+          <NavLink to='/' onClick={closeDrawer}>
+            <span>
+              <FaGauge size='20px' />
+            </span>
+            <span>Dashboard</span>
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to='/tasks' onClick={closeDrawer}>
+            <span>
+              <FaTasks size='20px' />
+            </span>
+            <span>Tasks</span>
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to='/settings' onClick={closeDrawer}>
+            <span>
+              <FaGear size='20px' />
+            </span>
+            <span>Settings</span>
+          </NavLink>
+        </li>
+      </ul>
+    </>
+  )
+}
+
+export default DrawerSideNav
