f85866b8381d5d65dc839baa4430536a284bdcac
Amka <Aminhassen1470@gmail.com>
2023-12-28 12:24:26 -0500

    display in-progress task data

--- src/pages/tasks/in-progress-tasks.tsx
+++ src/pages/tasks/in-progress-tasks.tsx
@@ -1,5 +1,31 @@
+import { useLoaderData } from 'react-router-dom'
+import { Task } from '@zod/tasksSchema'
+
 const InProgressTasks = () => {
-  return <div>InProgressTasks</div>
+  const inProgressTasks = useLoaderData() as Task[]
+
+  return (
+    <>
+      <section className='flex flex-col gap-10'>
+        <div className='max-w-md flex flex-col gap-2'>
+          <h1 className='md:text-2xl text-xl font-bold text-primary'>
+            In-Progress Tasks
+          </h1>
+          <p className='text-sm'>
+            In-progress tasks are tasks that you are currently working on.
+          </p>
+        </div>
+
+        <ul className='grid grid-col-1'>
+          {inProgressTasks.map((task) => (
+            <li className='card bg-neutral text-neutral-content' key={task.id}>
+              <div className='card-body'>{task.title}</div>
+            </li>
+          ))}
+        </ul>
+      </section>
+    </>
+  )
 }
 
 export default InProgressTasks
