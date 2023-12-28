a3efea23da65cb29533dd7f32bbb7da5088acef7
Amka <Aminhassen1470@gmail.com>
2023-12-28 04:33:50 -0500

    update `TaskNav` component

--- src/components/tasks/TasksNav.tsx
+++ src/components/tasks/TasksNav.tsx
@@ -1,27 +1,40 @@
+import { ComponentProps } from 'react'
 import { Link, useLocation } from 'react-router-dom'
+import { IoTimer } from 'react-icons/io5'
+import { IoIosCheckmarkCircle } from 'react-icons/io'
 
-export const TasksNav = () => {
+type TasksNavProps = ComponentProps<'div'> & {
+  categoryId?: string
+}
+
+export const TasksNav = ({ categoryId }: TasksNavProps) => {
   const location = useLocation()
 
   return (
-    <nav className=' flex items-center justify-center'>
-      <ul className='menu menu-horizontal gap-2 border-b border-b-neutral-500'>
-        <li>
+    <nav className='flex items-center justify-center'>
+      <ul className='menu menu-horizontal justify-center gap-2 border-b border-b-neutral-500'>
+        <li className='sm:w-fit w-full'>
           <Link
-            to='/tasks'
-            className={`${location.pathname === '/tasks' ? 'active' : ''}`}
-          >
-            In Progress
-          </Link>
-        </li>
-        <li>
-          <Link
-            to='/tasks/completed'
+            to={`/tasks/${categoryId}`}
             className={`${
-              location.pathname === '/tasks/completed' ? 'active' : ''
+              location.pathname === `/tasks/${categoryId}` ? 'active' : ''
             }`}
           >
-            Completed
+            <IoTimer />
+            <span>In Progress</span>
+          </Link>
+        </li>
+        <li className='sm:w-fit w-full'>
+          <Link
+            to={`/tasks/${categoryId}/completed`}
+            className={`${
+              location.pathname === `/tasks/${categoryId}/completed`
+                ? 'active'
+                : ''
+            }`}
+          >
+            <IoIosCheckmarkCircle />
+            <span>Completed</span>
           </Link>
         </li>
       </ul>
