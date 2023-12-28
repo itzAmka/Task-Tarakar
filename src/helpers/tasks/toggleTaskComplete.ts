41b944ea4a2c528609df4b39b5d1ccc03cda4809
Amka <Aminhassen1470@gmail.com>
2023-12-28 15:48:23 -0500

    create a function to toggle "complete" task

---
+++ src/helpers/tasks/toggleTaskComplete.ts
@@ -0,0 +1,13 @@
+import { doc, updateDoc } from 'firebase/firestore'
+import { db } from '@config/firebase.config'
+import { Task } from '@zod/tasksSchema'
+
+export const toggleTaskComplete = async (task: Task) => {
+  const { id, isCompleted } = task
+
+  const taskDocRef = doc(db, 'tasks', id)
+
+  await updateDoc(taskDocRef, {
+    isCompleted: !isCompleted,
+  })
+}
