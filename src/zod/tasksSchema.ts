aaa337dad4379b3d5e1369faec1b27f8e33c4a60
Amka <Aminhassen1470@gmail.com>
2023-12-27 19:47:39 -0500

    update tasks schema

--- src/zod/tasksSchema.ts
+++ src/zod/tasksSchema.ts
@@ -4,6 +4,7 @@ export const tasksSchema = z.object({
   id: z.string(),
   title: z.string(),
   isCompleted: z.boolean().default(false),
+  userRef: z.string(),
   categoryRef: z.string(),
   createdAt: z.string(),
   updatedAt: z.string(),
