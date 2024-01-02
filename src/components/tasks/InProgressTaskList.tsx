6634976cd9b347f9787f3b083640bd3554657f34
Amka <Aminhassen1470@gmail.com>
2024-01-02 18:57:57 -0500

    update create task link in `CompletedTaskList` and `InProgressTaskList`

--- src/components/tasks/InProgressTaskList.tsx
+++ src/components/tasks/InProgressTaskList.tsx
@@ -1,7 +1,7 @@
 import { ComponentProps } from 'react'
 import { Task } from '@zod/tasksSchema'
 import { InProgressTaskItem } from '@components/tasks'
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 import { IoAddCircle } from 'react-icons/io5'
 
 type InProgressTaskListProps = ComponentProps<'ul'> & {
@@ -11,6 +11,10 @@ type InProgressTaskListProps = ComponentProps<'ul'> & {
 export const InProgressTaskList = ({
   inProgressTasks,
 }: InProgressTaskListProps) => {
+  const location = useLocation()
+
+  const categoryRef = location.pathname.split('/').pop()
+
   return (
     <>
       <ul className='grid grid-col-1 gap-4'>
@@ -23,7 +27,10 @@ export const InProgressTaskList = ({
         ) : (
           <li className='brightness-75 flex flex-col gap-4'>
             <p>You have no in-progress tasks on this category.</p>
-            <Link to='/tasks' className='btn btn-sm btn-success w-fit'>
+            <Link
+              to={`/tasks/${categoryRef}/create-task`}
+              className='btn btn-sm btn-success w-fit'
+            >
               <span className='sm:inline-block hidden'> Create a task</span>
               <IoAddCircle size='30px' />
             </Link>
